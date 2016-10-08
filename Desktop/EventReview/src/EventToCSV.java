1e5840e92f3e40b43b16f1eaa808361757f723e5
ryosukeminami <ryosukem13@gmail.com>
2016-10-08 14:33:29 +0100

    change

--- Desktop/EventReview/src/EventToCSV.java
+++ Desktop/EventReview/src/EventToCSV.java
@@ -8,8 +8,8 @@ public class EventToCSV {
     public static void createCSV() {
         try {
             PrintWriter writer = new PrintWriter("events.csv");
-            //writer.println("name,date,costOfTicket,organizer");
-            //writer.close();
+            // writer.println("name,date,location,costOfTicket,organizer");
+            // writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -17,8 +17,8 @@ public class EventToCSV {
 
     public static void addEvent(Event event) {
         StringBuilder entryBuilder = new StringBuilder();
-        entryBuilder.append(event.getName() + "," + event.getDate() + "," +
-                event.getCostOfTicket() + "," + event.getOrganizer());
+        entryBuilder.append(event.getName() + "," + event.getDate() + "," + event.getLocation() +
+                "," + event.getCostOfTicket() + "," + event.getOrganizer());
 
         PrintWriter writer = null;
 
