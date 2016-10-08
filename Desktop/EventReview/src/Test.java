ba0ade5443929397ce2b7191ce4194b01c47f455
kreckert <kreckert@mac.com>
2016-10-08 15:03:10 +0100

    init

--- Desktop/EventReview/src/Test.java
+++ Desktop/EventReview/src/Test.java
@@ -4,6 +4,8 @@
 public class Test {
 
     public static void main(String[] args) {
+
+        /*
         Event hackTheBubble = new Event("Hack the Bubble");
         hackTheBubble.setDate("08-10-2016");
         hackTheBubble.setLocation("Jack Cole");
@@ -17,5 +19,24 @@ public class Test {
         CSVParser parser = new CSVParser();
 
         parser.parseEvents("events.csv");
+        */
+
+        for (int i = 0; i < 100; i++) {
+            //System.out.println(generateCost());
+            System.out.println(generateDate());
+        }
+    }
+
+    public static int generateCost() {
+
+        return (int) Math.floor(Math.random() * 50);
+    }
+
+    public static String generateDate() {
+
+        int day = (int) Math.floor(Math.random() * 30) + 1;
+        int month = (int) Math.floor(Math.random() * 12) + 1;
+
+        return day + "-" + month + "-" + "2016";
     }
 }
