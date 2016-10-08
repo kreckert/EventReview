3631cd2be0855df902d151a210001d4389a74d70
kreckert <kreckert@mac.com>
2016-10-08 12:57:41 +0100

    init

--- Desktop/EventReview/src/Event.java
+++ Desktop/EventReview/src/Event.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
+
 /**
  * Created by kylereckert on 10/8/16.
  */
 public class Event {
 
-    private int rating;
-    private int numOfRatings;
     private String name;
+    private ArrayList<EventRating> ratings = new ArrayList<>();
 
     //criteria for rating
     private int friendsGoing;
@@ -15,8 +16,6 @@ public class Event {
     public Event(String name) {
 
         this.name = name;
-        rating = 0;
-        numOfRatings = 0;
     }
 
     public String getName() {
@@ -35,12 +34,24 @@ public class Event {
         return organizer;
     }
 
-    public void addRating(int userRating) {
+    public void addRating(int score, String author) {
 
-        if (userRating >= 0 && userRating <= 1) {
+        EventRating rating = new EventRating(score, author);
+    }
 
-            rating = ((rating * numOfRatings) + userRating)/(numOfRatings + 1);
-            numOfRatings++;
+    public void addRating(int score, String comment, String author) {
+
+        EventRating rating = new EventRating(score, comment, author);
+    }
+
+    public int getScore() {
+        if (ratings.size() == 0)
+            return -1;
+
+        int score = 0;
+        for (int i = 0; i < ratings.size(); i++) {
+            score += ratings.get(i).getScore();
         }
+        return (score/ratings.size());
     }
 }
--- Desktop/EventReview/src/EventRating.java
+++ Desktop/EventReview/src/EventRating.java
@@ -5,5 +5,28 @@ public class EventRating {
 
     private int score;//0 to 10
     private String comment;
-    private Event event;
+    private String Author;
+
+    public EventRating(int score, String author) {
+        this.score = score;
+        Author = author;
+    }
+
+    public EventRating(int score, String comment, String author) {
+        this.score = score;
+        this.comment = comment;
+        Author = author;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
 }
