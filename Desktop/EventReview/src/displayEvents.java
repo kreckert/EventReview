c995fb9550e2573350d35d2cd464d0f94d5c7ffd
la57 <louisahmed98@gmail.com>
2016-10-08 12:18:54 +0100

    displayEvents

---
+++ Desktop/EventReview/src/displayEvents.java
@@ -0,0 +1,13 @@
+/**
+ * Created by la57 on 08/10/16.
+ */
+public class displayEvents {
+    public static void main(String[] args){
+        int length = 5;
+        String[] events = new String[length];
+        for (int i = 0 ; i<length ; i++){
+            events[i] = "";
+        }
+
+    }
+}
