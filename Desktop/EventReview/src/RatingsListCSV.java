27d1924dfefd845cc31a45c03b050e6449d75d35
Ryosuke Minami <ryosukem13@gmail.com>
2016-10-08 19:09:50 +0100

    uploading from IntelliJ

--- Desktop/EventReview/src/RatingsListCSV.java
+++ Desktop/EventReview/src/RatingsListCSV.java
@@ -9,56 +9,6 @@ import java.io.PrintWriter;
 public class RatingsListCSV {
 
     /**
-    public static void createCSVForEvent(Event event) {
-
-        String eventName = event.getName().replaceAll("\\s+", "-").toLowerCase();
-        PrintWriter writer = null;
-
-        try {
-            writer = new PrintWriter("ratings-for-" + eventName + ".csv");
-            writer.println("event-name,author,score,comment");
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-    }
-
-    public static void addRating(EventRating rating) {
-
-        String eventName = rating.getEventName().replaceAll("\\s+", "-").toLowerCase();
-        PrintWriter writer = null;
-
-        try {
-            writer = new PrintWriter(new FileWriter("ratings-for-" + eventName + ".csv", true));
-
-            StringBuilder entry = new StringBuilder();
-            entry.append(rating.getEventName() + ",");
-            entry.append(rating.getAuthor() + ",");
-            entry.append(rating.getRatingScore() + ",");
-            entry.append(rating.getComment());
-
-            writer.println(entry.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Event hackTheBubble = new Event("Hack the Bubble");
-        createCSVForEvent(hackTheBubble);
-    }
-     */
-
-    /**
      * creates new files named eventRatings.csv
      *
      */
