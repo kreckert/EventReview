9c5ad8198694e865b262790be507c9e7d0cb6423
Ryosuke Minami <ryosukem13@gmail.com>
2016-10-08 18:51:54 +0100

    uploading from IntelliJ

--- Desktop/EventReview/src/EventToCSV.java
+++ Desktop/EventReview/src/EventToCSV.java
@@ -12,8 +12,6 @@ public class EventToCSV {
     public static void createCSV() {
         try {
             PrintWriter writer = new PrintWriter("events.csv");
-            // writer.println("name,date,location,costOfTicket,organizer");
-            // writer.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
