00f1775ad7af97248d7cca99e4b1dde321dee987
ryosukeminami <ryosukem13@gmail.com>
2016-10-08 13:04:00 +0100

    change

--- Desktop/EventReview/src/EventRating.java
+++ Desktop/EventReview/src/EventRating.java
@@ -6,4 +6,16 @@ public class EventRating {
     private int score;//0 to 10
     private String comment;
     private Event event;
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
 }
